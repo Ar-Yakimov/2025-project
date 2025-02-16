2ed913d8974128aaa160fb5b0e06c0143fc82760
Arseniy Yakimov <ar.yakimov@gmail.com>
2025-02-16 23:53:26 +0300

    ready to add profile page

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 from os import environ
+
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, abort, flash
+from flask import Flask, render_template, request, redirect, abort, flash, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_talisman import Talisman
 from werkzeug.security import generate_password_hash, check_password_hash
 
-load_dotenv("/instance/config.env")
+load_dotenv(r"instance/config.env")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URI")
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_PERMANENT'] = False
 database = SQLAlchemy(app)
-with app.app_context():
-    database.create_all()
 
 
 class Note(database.Model):
@@ -41,6 +42,9 @@ class User(database.Model):
     def __repr__(self):
         return f"<User {self.id}>"
 
+
+with app.app_context():
+    database.create_all()
 
 csp = {
     'default-src': r"'self'",
@@ -70,98 +74,165 @@ def index():
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_note():
-    if request.method == "GET":
-        return render_template("create.html")
-    else:
+    if 'user_id' not in session:
+        abort(403)
+
+    if request.method == "POST":
         title = request.form['title']
         text = request.form['text']
+        user_id = session['user_id']
+        new_note = Note(title=title, text=text, user_id=user_id)
 
-        new_note = Note(title=title, text=text)
         try:
             database.session.add(new_note)
             database.session.commit()
-            return redirect('/notes')
+            return redirect(url_for('show_notes'))
         except Exception as error:
             return f"Произошла ошибка: {error}"
+    else:
+        return render_template("create.html")
 
 
 @app.route("/notes")
 def show_notes():
-    notes = Note.query.order_by(Note.date.desc()).all()
+    if 'user_id' not in session:
+        abort(403)
+
+    user_id = session['user_id']
+    notes = Note.query.filter_by(user_id=user_id).order_by(Note.date.desc()).all()
     return render_template("notes.html", notes=notes)
 
 
 @app.route("/notes/<int:id>")
 def read_note(id):
+    if 'user_id' not in session:
+        abort(403)
+
     note = database.session.get(Note, id)
+
+    if note is None:
+        abort(404)
+
+    if note.user_id != session['user_id']:
+        abort(403)
+
     return render_template("note_detailed.html", note=note)
 
 
 @app.route("/notes/<int:id>/del")
 def remove_note(id):
+    if 'user_id' not in session:
+        abort(403)
+
     note = database.session.get(Note, id)
+
+    if note is None:
+        abort(404)
+
+    if note.user_id != session['user_id']:
+        abort(403)
 
     try:
         database.session.delete(note)
         database.session.commit()
-        return redirect("/notes")
+        return redirect(url_for('show_notes'))
     except Exception as error:
         return f"Произошла ошибка: {error}"
 
 
 @app.route("/notes/<int:id>/upd", methods=["POST", "GET"])
 def change_note(id):
-    note = database.session.get(Note, id)
-    if request.method == "POST":
-        note.title = request.form['title']
-        note.text = request.form['text']
+    if 'user_id' not in session:
+        abort(403)
 
-        try:
-            database.session.commit()
-            return redirect('/notes')
-        except Exception as error:
-            return f"Произошла ошибка: {error}"
-    else:
-        return render_template("update.html", note=note)
+    note = database.session.get(Note, id)
+
+    if note is None:
+        abort(404)
+
+    if note.user_id != session['user_id']:
+        abort(403)
+
+    try:
+        database.session.delete(note)
+        database.session.commit()
+        return redirect(url_for('show_notes'))
+    except Exception as error:
+        return f"Произошла ошибка: {error}"
 
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    if request.method == "GET":
-        abort(403)
-    else:
-        new_login = request.form["login"]
-        new_password = request.form["password"]
+    if request.method == "POST":
+        new_login = request.form["new_login"]
+        new_password = request.form["new_password"]
 
-        if database.session.query(User).filter_by(login=new_login).all():
+        if database.session.query(User).filter_by(login=new_login).first():
             flash("Пользователь уже существует!")
+            return redirect(url_for('index'))
         else:
             try:
-                new_user = User(login=new_login, password=User.set_password(new_password))
+                new_user = User(login=new_login)
+                new_user.set_password(new_password)
                 database.session.add(new_user)
                 database.session.commit()
+                return redirect(url_for('notes'))
             except Exception as error:
                 return f"Произошла ошибка: {error}"
-        return redirect("/")
+    else:
+        abort(403)
 
 
 @app.route("/sing-in", methods=["GET", "POST"])
 def sing_in():
-    if request.method == "GET":
-        abort(403)
+    if request.method == "POST":
+        login = request.form["old_login"]
+        password = request.form["old_password"]
+        user = User.query.filter_by(login=login).first()
+
+        if not user:
+            flash("Неверный логин!")
+            return redirect(url_for('index'))
+
+        if not user.check_password(password):
+            flash("Неверный пароль!")
+            return redirect(url_for('index'))
+
+        session['user_id'] = user.id
+        return redirect(url_for('show_notes'))
     else:
-        login = request.form["login"]
-        password = request.form["password"]
+        abort(403)
 
-        if check := database.session.query(User).filter_by(login=login).first():
-            if check.check_password(password):
-                USER = check.id
-            else:
-                flash("Неверный пароль!")
-        else:
-            flash("Несуществующий логин!")
 
-        return redirect("/")
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("index"))
+
+
+@app.route("/profile")
+def info():
+    if 'user_id' not in session:
+        abort(403)
+
+    user_id = session['user_id']
+    user = User.query.get(user_id)
+
+    if user:
+        return render_template("profile.html", user=user)
+    else:
+        flash("Пользователь не найден")
+        return redirect(url_for("index"))
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return "Ошибка 403: У вас нет доступа к этому url!"
+
+
+@app.errorhandler(404)
+def dont_exist(error):
+    return "Ошибка 404: Данный url не существует!"
 
 
 if __name__ == "__main__":
